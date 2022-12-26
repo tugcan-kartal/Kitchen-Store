eb80622b8ccd8440bbd6c070cbbb76f622c29ed3
tugcan-kartal <tugcankartal01@gmail.com>
2022-12-26 22:56:23 +0300

    commit for back-up

--- src/components/Discounts.js
+++ src/components/Discounts.js
@@ -1,19 +1,19 @@
 import React,{useState,useEffect} from 'react';
 import Slider from 'react-slick';
 import Banners from '../api/Banners.json';
-import { IoIosArrowBack, IoIosArrowForward } from 'react-icons/io'
-import { useWindowWidth } from '@react-hook/window-size'
+import { IoIosArrowBack, IoIosArrowForward } from 'react-icons/io';
+import { useWindowWidth } from '@react-hook/window-size';
 
 function NextButton ({ onClick, className }) {
 	return (
-		<button className={`${className} text-purple-700`} onClick={onClick}>
+		<button className={`${className} text-blue-700`}  onClick={onClick}>
 			<IoIosArrowForward size={22} />
 		</button>
 	)
 }
 function PrevButton ({ onClick, className }) {
 	return (
-		<button className={`${className} text-purple-700`} onClick={onClick}>
+		<button className={`${className} text-blue-700`} onClick={onClick}>
 			<IoIosArrowBack size={22} />
 		</button>
 	)
@@ -56,15 +56,15 @@ function Discounts() {
 
   return (
     <>
-      <div className='mt-32 w-1/2 m-auto'>
+      <div className='mt-32 w-3/4 m-auto'>
         <h1 className='text-2xl font-bold text-blue-800'>Recent Discounts</h1>
         <Slider {...settings}>
           {banners && banners.map(banner=>(
             <div className='bg-white rounded-lg shadow-md text-center' key={banner.id}>
               <img className='w-full scale-75' src={banner.image} />
               <h1 className='font-semibold text-lg mb-2'>{banner.header}</h1>
-              <h4 className='font-light mb-2'>{banner.content}</h4>
-              <h6 className='font-bold text-md mb-2'>${banner.price}</h6>
+              <h4 className='mb-2 font-semibold text-md text-blue-800'>{banner.content}</h4>
+              <h6 className='font-bold text-md mb-2 text-xl'>${banner.price}</h6>
             </div>
           ))}
         </Slider>
